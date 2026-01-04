e1ef5c765d6b1d7bff6f8d508031ba79098423cf
prit2006 <pritpastagiya2006@gmail.com>
2026-01-04 16:26:47 +0530

    exploring the get post

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express();
 const port = 3000;
-
+app.get("/user", (req, res) => {
+    res.send("Prit Pastagiya")
+})
+app.post("/user", (req, res) => {
+    res.send("Post request on user")    
+})
 app.use("/test", (req, res) => {
     res.send("Server started!")
 })
